36a70210788932075b5de985028823b75e12991c
Abhishek Gupta <hi@itsabhishek.me>
2019-09-20 18:54:56 +0530

    load courses visible to hello interns only

--- onlinecb/pages/courses/index.vue
+++ onlinecb/pages/courses/index.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    Hello from courses
+    <ul>
+      <li v-for="course in courses" :key="course.id"> {{ course.title }} </li>
+    </ul>
   </div>
 </template>
+
+
+<script>
+export default {
+  async asyncData ({ $axios, app }) {
+    const res = await $axios.get('/courses', {
+      params: {
+        exclude: `ratings,instructors.*`,
+        include: `instructors,runs`,
+        filter: {
+          unlisted: false,
+          domains: {
+            $contains: ['hellointern']
+          }
+        },
+        page: {
+          limit: 100
+        }
+      }
+    })
+    const courses = app.$store.sync(res.data)
+    return  { courses }
+  },
+  data () {
+    return {
+      courses: [] 
+    }
+  }
+}
+</script>
